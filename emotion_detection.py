5be7e3822236f694f2ed2c4fbd11ff2388b352de
Anas <pm@mapka.com>
2026-02-28 19:01:05 -0500

    Task 3: format output and add dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,32 @@
 import requests
+import json
 
 URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
 def emotion_detector(text_to_analyze):
-    payload = { "raw_document": { "text": text_to_analyze } }
+    payload = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(URL, json=payload, headers=HEADERS)
-    return response.text
+
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+    anger = emotions["anger"]
+    disgust = emotions["disgust"]
+    fear = emotions["fear"]
+    joy = emotions["joy"]
+    sadness = emotions["sadness"]
+
+    dominant_emotion = max(
+        {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness},
+        key=lambda k: {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness}[k]
+    )
+
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
